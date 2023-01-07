f2c83c01919c08e8c70241e76eec597d5ed58ebd
Luda <108550003+snowwhitesheep@users.noreply.github.com>
2023-01-07 15:07:00 +0100

    Update course7_week3_tasks.c

--- course7_week3_tasks.c
+++ course7_week3_tasks.c
@@ -18,3 +18,30 @@ printf("%d plants for %.2lf dollars each cost %.2lf dollars.", plants, price, pl
 }else {
 printf("Invalid input");}
 return 0;}
+
+/*TASK 2
+You are teaching a class on C-programming! You would like to find out whether your teaching has been effective, and so you need to compute 
+the average grade your students have received on the most recent assignment. These grades (integers) are stored in the file studentGrades.txt.
+
+The first entry in the file (an integer) is the number of student grades that are stored in the file.
+Your job is to calculate and print out the average of the grades stored in the file. 
+Please print the average grade with two decimal places.
+*/
+
+#include <stdio.h>
+
+int main (void){
+int N, i;
+double avarage=0;
+double grade;
+FILE *file;
+
+file = fopen("studentGrades.txt", "r");
+fscanf(file, "%d", &N);
+for (i=0; i<N; i++){
+fscanf(file, "%lf", &grade);
+avarage += grade;}
+avarage = avarage/N;
+printf("%.2lf", avarage);
+
+return 0;}
